354564bfe0217ec7fc07fca23638ff4c9496d685
mufa2906 <m.farhan2906@gmail.com>
2021-09-12 11:27:01 +0700

    menambahkan fitur INSERT data

--- wpu/pertemuan10/latihan3.php
+++ wpu/pertemuan10/latihan3.php
@@ -17,7 +17,8 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
 <body>
   <h3>Daftar Mahasiswa</h3>
 
-
+  <a href="tambah.php">Tambah Data Mahasiswa</a>
+  <br><br>
   <table border="1" cellspacing="10" cellpadding="">
 
     <tr>
