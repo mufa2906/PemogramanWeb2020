44dfdcaf938b24bd5f476b107f093a4e4201b654
mufa2906 <m.farhan2906@gmail.com>
2021-09-14 13:06:04 +0700

    pertemuan 11(ubah data dan hapus data)

---
+++ wpu/pertemuan11/index.php
@@ -0,0 +1,57 @@
+<?php
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+//Ketika tombol cari diklik
+if (isset($_POST["cari"])) {
+  $mahasiswa = cari($_POST["keyword"]);
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <a href="tambah.php">Tambah Data Mahasiswa</a>
+  <br><br>
+
+  <form action="" method="POST">
+    <input type="text" name="keyword" size="40" placeholder="masukkan keyword pencarian.." autocomplete="off" autofocus>
+    <button type="submit" name="cari">Cari!</button>
+  </form>
+  <br>
+  <table border="1" cellspacing="10" cellpadding="">
+
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Nama</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php $num = 1;
+    foreach ($mahasiswa as $m) : ?>
+    <tr>
+      <td><?= $num++ ?></td>
+      <td><img src="img/<?= $m['gambar']; ?>" width="60" height="80"></td>
+      <td><?= $m['nama']; ?></td>
+      <td>
+        <a href="detail.php?id=<?= $m['id']; ?>">lihat detail</a>
+      </td>
+    </tr>
+    <?php endforeach; ?>
+
+</body>
+
+</html>
